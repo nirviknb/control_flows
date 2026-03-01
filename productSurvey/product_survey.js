c3d456ae642b3daabb78870083b57630d5ca6682
nirviknb <nirvik.nb@gmail.com>
2026-03-01 04:34:17 -0500

    addded user experiece to the  survey

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -8,7 +8,7 @@ function submitFeedback(){
     const designation=document.getElementById('designation').value;
     const productType=document.getElementById('productType').value;
     const feedback=document.getElementById('feedbackText').value;
-
+    const userExperiece=document.getElementById('userExperience').value;
     alert('Thank you for your valuable feedback');
 
     document.getElementById('userName').innerHTML=username;
@@ -18,6 +18,7 @@ function submitFeedback(){
     document.getElementById('userDesignation').innerHTML=designation;
     document.getElementById('userProductChoice').innerHTML=productType;
     document.getElementById('userFeedback').innerHTML=feedback;
+    document.getElementById('usersExperience').innerHTML=userExperiece;
 
     document.getElementById('userInfo').style.display='block';
 }
